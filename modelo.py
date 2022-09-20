b61a965886047fa5f65d2f4c11ec9308403d4204
Jonathan <jonathanjacobovski@alunos.utfpr.edu.br>
2022-09-20 11:13:31 -0300

    Playlist

--- modelo.py
+++ modelo.py
@@ -38,17 +38,45 @@ class Serie(Programa):
     def __str__(self):
         return f'Nome: {self._nome} - {self.temporadas} - Likes: {self.likes}'
 
+class Playlist(list):
+    def __init__(self, nome,Programa):
+        self.nome = nome
+        super().__init__(Programa)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme("Todo mundo em pânico", 1999, 100)
+demolidor = Serie("Demolidor", 2016, 2)
+
 
 vingadores.dar_likes()
 vingadores.dar_likes()
 vingadores.dar_likes()
-
+demolidor.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+tmep.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
 atlanta.dar_likes()
 atlanta.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
 
-listinha = [vingadores, atlanta]
+listinha = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist(f'Fim de semana', listinha)
+
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
 
 for Programa in listinha:
     print(Programa)
+
