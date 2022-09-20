b263990b1bc9050b33126f4b06443f757fc02dcf
Jonathan <jonathanjacobovski@alunos.utfpr.edu.br>
2022-09-20 08:24:40 -0300

    Playlist

---
+++ modelo.py
@@ -0,0 +1,54 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return f'Nome: {self._nome} Likes: {self.likes}'
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    def __str__(self):
+        return f'Nome: {self._nome} - {self.duracao} - Likes: {self.likes}'
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+    def __str__(self):
+        return f'Nome: {self._nome} - {self.temporadas} - Likes: {self.likes}'
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+listinha = [vingadores, atlanta]
+
+for Programa in listinha:
+    print(Programa)
